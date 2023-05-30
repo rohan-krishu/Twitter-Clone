51024cc82bcda642142af8a644ecc6966dc6fd6e
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-31 02:18:07 +0530

    Added Redex got sign-up page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './Components/pages/Home/Home';
-
 import GotoSignUp from './Components/GotoSignUp/GotoSignUp';
 import SignUpPage from './Components/Register/SignUpPage';
 import SignInPage from './Components/SignIn/SignIn';
+import { store } from './Components/Store/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={<SignInPage/>} />
-        <Route path='/enter-page' element={<GotoSignUp />} />
-        <Route path='/sign-up' element={<SignUpPage />} />
-      </Routes>
+      <Provider store={store}> 
+        <Routes >
+          <Route path='/' element={<Home />} />
+          <Route path='/sign-in' element={<SignInPage />} />
+          <Route path='/enter-page' element={<GotoSignUp />} />
+          <Route path='/sign-up' element={<SignUpPage />} />
+        </Routes>
+      </Provider>
+
     </div>
   );
 }
--- src/Components/Store/authSlice.js
+++ src/Components/Store/authSlice.js
@@ -1,6 +1,37 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import { useState } from 'react'
 
-// const authSlice = createSlice({
-//     name: 'auth',
-//     initialState: 
-// })
+// const initialState = false
+
+// const [data,setData] = useState('')
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        userName: '',
+        phone: '',
+        email: '',
+        date: '',
+    }, //initialValue
+
+    reducers: {  //settlerFunction
+        setUserName: ((state, action) => {
+            state.userName = action.payload
+        }),
+        setPhone: ((state, action) => {
+            state.phone = action.payload
+        }),
+        setEmail: ((state, action) => {
+            state.email = action.payload
+        }),
+        setDate: ((state, action) => {
+            state.date = action.payload
+        }),
+
+    }
+})
+
+const { setDate, setPhone, setEmail, setUserName } = authSlice.actions
+
+export default authSlice.reducer
+
+export { setDate, setPhone, setEmail, setUserName } 
