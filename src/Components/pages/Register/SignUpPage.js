64fb6589d7dc5b8c41e6adc7c2081515462d5986
ashish@123456 <ashishkhankari0927@gmail.com>
2023-05-31 20:26:31 +0530

    Improved CSS for SignUp Component

--- src/Components/pages/Register/SignUpPage.js
+++ src/Components/pages/Register/SignUpPage.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import "./SignUpPage.css";
+import { useState } from "react";
 import { BsTwitter } from 'react-icons/bs';
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -11,13 +12,16 @@ import { setDate, setPhone, setEmail, setUserName } from '../../Store/authSlice'
 function SignUpPage() {
   //state for using navigate to pages
   const navigate = useNavigate()
-
+  const [useEmail, setUseEmail] = useState(false)
   //selector for data assigning
   const { date, phone, email, userName } = useSelector(state => state.auth)
 
   //dispatch data to authSlice
   const dispatch = useDispatch()
 
+  function toggleFunction() {
+    setUseEmail(!useEmail)
+  }
   //function to get userData
   function submitSignUpData(e) {
     e.preventDefault()
@@ -62,32 +66,46 @@ function SignUpPage() {
 
           <br /><br /><br />
 
-          <TextField
-            className="outlined-basic"
-            label="Phone"
-            variant="outlined"
-            type="number"
-            onChange={(e) => dispatch(setPhone(e.target.value))}
-            value={phone}
-          />
 
-          <input
-            placeholder="Email"
-            type='email'
-            onChange={(e) => dispatch(setEmail(e.target.value))}
-            value={email}
-          />
+
+          <div>
+            {useEmail ?
+              <TextField
+                className="outlined-basic"
+                label="Phone"
+                variant="outlined"
+                type="number"
+                onChange={(e) => dispatch(setPhone(e.target.value))}
+                value={phone}
+              />
+              :
+              <TextField
+                className="outlined-basic"
+                label="Email"
+                variant="outlined"
+                type="email"
+                onChange={(e) => dispatch(setEmail(e.target.value))}
+                value={email}
+              />
+              // "Hello"
+
+            }
+
+            <Link className="toggleLink" onClick={toggleFunction}>{useEmail ? "Use Email Instead" : "Use Phone Instead"}</Link>
+
+          </div>
           <br />
 
-          <p>User email instead</p>
+          <div className="text">
+            <h3>Date of birth</h3>
+            <p>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</p>
 
-          <h3>Date of birth</h3>
-          <p>This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.</p>
-{/* 
+          </div>
+          
           <TextField
-
-          /> */}
-          <input
+            className="outlined-basic"
+            label="Date"
+            variant="outlined"
             type="date"
             placeholder="DOB"
             onChange={(e) => dispatch(setDate(e.target.value))}
@@ -95,7 +113,7 @@ function SignUpPage() {
           />
 
           <div className="navigate-button">
-          <Link to={'/sign-in'} onClick={submitSignUpData}><button className="next">Next</button></Link>
+            <Link to={'/sign-in'} onClick={submitSignUpData}><button className="next">Next</button></Link>
           </div>
         </form>
 
