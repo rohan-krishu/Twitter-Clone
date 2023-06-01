b4b6bf2e6b7ce36a58a54547f4816cd1a134bf85
ROHAN-W11 <rohanrathi333@gmail.com>
2023-06-01 11:42:04 +0530

    backup twitter clone project

--- src/Components/pages/Home/Home.js
+++ src/Components/pages/Home/Home.js
@@ -1,19 +1,32 @@
+import './Home.module.css'
 import React from 'react'
 import WhatsHappening from './HomePages/WhatsHappening/WhatsHappening'
 import SearchBar from './HomePages/SearchBar/SearchBar'
 import styles from './Home.module.css'
+import Sidebar from './HomePages/Left-SideBar/Sidebar'
 import AddTweet from './HomePages/AddTweetSection/AddTweet'
+
+
 export default function Home() {
   return (
-    <div className={styles.CenterSection}>
-       <AddTweet/>
+    <div className={styles.combinedSection} >
+      <div className= {styles.leftSection}>
+       <Sidebar/>
+      </div>
+      
+
+      <div className={styles.CenterSection}>
+        <AddTweet />
+        Helllo Twitter i making
+        
+        {/* Suraj Call Here */}
+      </div>
       <div className={styles.RightSection}>
         <SearchBar />
         <div className={styles.whatsHappening}>
           <WhatsHappening />
         </div>
       </div>
-     
     </div>
   )
 }
